915e0f4d744afa63f44373937cb1ff5c81c48262
seungjae yoon <tmdwo5811@naver.com>
2020-01-27 15:25:18 +0900

    비밀번호 인코드 기능 구현 완료

--- src/main/java/com/palace/bowling_prj/controller/LoginController.java
+++ src/main/java/com/palace/bowling_prj/controller/LoginController.java
@@ -1,8 +1,13 @@
 package com.palace.bowling_prj.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,9 +22,16 @@ public class LoginController {
 		// 로그인페이지 접속
 		return "joinForm";
 	}
-	
-	@RequestMapping("/loginPage")
-	public String loginPage() {
-		return "loginPage";
+	@RequestMapping("/userJoin")
+	public String userJoin(HttpServletRequest request) {
+		// 회원가입 실행
+		BCryptPasswordEncoder passEncoder = new BCryptPasswordEncoder();
+		String userId = request.getParameter("userId");
+		String encode = passEncoder.encode(request.getParameter("userPassWord"));
+		System.out.println("사용 아이디 =>"+userId);
+		System.out.println("암호화된 비밀번호 =>"+encode);
+		
+		return "redirect:index";
 	}
+	
 }
--- src/main/webapp/WEB-INF/views/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/joinForm.jsp
@@ -7,10 +7,9 @@
 <title>회원 가입 페이지</title>
 </head>
 <body>
-	<form action="" method="post">
-	사용할 아이디 : <input type="text"><br>
-	사용할 비밀번호 : <input type="password"><br>
-	비밀번호 재확인 : <input type="password"><br>
+	<form action="userJoin" method="post">
+	사용할 아이디 : <input type="text" name="userId"><br>
+	사용할 비밀번호 : <input type="password" name="userPassWord"><br>
 	<input type="submit" value="회원가입">
 	</form>
 </body>
