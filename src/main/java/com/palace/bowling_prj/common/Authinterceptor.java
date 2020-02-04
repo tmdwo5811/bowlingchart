bbe188b22e42749bc0b87668699ce8223b22c411
seungjae yoon <tmdwo5811@naver.com>
2020-02-04 16:38:48 +0900

    Sant_J 브랜치 생성

--- src/main/java/com/palace/bowling_prj/common/Authinterceptor.java
+++ src/main/java/com/palace/bowling_prj/common/Authinterceptor.java
@@ -24,6 +24,7 @@ public class Authinterceptor extends HandlerInterceptorAdapter{
 			return true;
 		}
 	}
+	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
