409e0798f7303d8fb1d4685741dca816a66e6642
seungjae yoon <tmdwo5811@naver.com>
2020-02-27 13:47:38 +0900

    로그인창 유효성 검사 alert 추가

--- src/main/webapp/resources/script/dalivation.js
+++ src/main/webapp/resources/script/dalivation.js
@@ -1,7 +1,16 @@
 //회원가입 폼 유효성 검사
 
 //로그인 폼 유효성 검사
-
+function loginFormCheck(){
+	var form = document.loginForm;
+	if(!form.aId.value || !form.aPw.value){
+		alert("아이디 또는 비밀번호를 입력해주세요.")
+		form.aId.value.focus();
+		location.replace("index");
+	}else{
+		form.submit();
+	}
+}
 //사이즈 입력 폼 유효성 검사
 
 //비밀번호 찾기 폼 유효성 검사
